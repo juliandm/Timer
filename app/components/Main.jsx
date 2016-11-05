de45e666912aaaf497c4ca039d37a2c4b929c20e
Julian <juliandemourgues@gmail.com>
2016-11-05 17:05:51 +0100

    countdown

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,19 +1,17 @@
 var React = require('react');
 var Nav = require('Nav');
-var Clock = require('Clock');
 
 var Main = (props) => {
-  return(
-     <div>
-       <div >
-         <div>
-           <Nav/>
-           <p>Main rendered</p>
-            {props.children}
-         </div>
-       </div>
-     </div>
-  )
+  return (
+    <div>
+      <Nav/>
+      <div className="row">
+        <div className="column small-centered medium-6 large-4">
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+}
 
-};
 module.exports = Main;
